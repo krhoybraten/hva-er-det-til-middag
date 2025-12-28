9eaa514190418a491caf04c674ba12193d1bda80
krhoybraten <106737615+krhoybraten@users.noreply.github.com>
2025-12-28 13:59:09 +0100

    Add basic random dinner generator to main.js

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
+const middag = document.getElementById("middag");
+const generate = document.getElementById("generate");
 
+const dinners = ['Hamburger', 'Pizza', 'Taco'];
+
+function getRandomDinner(dinnerList) {
+  const dinnerIndex = Math.floor(Math.random() * dinnerList.length);
+  return dinnerList[dinnerIndex];
+}
+
+function replaceText(element, newText) {
+  element.textContent = newText;
+}
+
+generate.addEventListener('click', () => {
+  replaceText(middag, getRandomDinner(dinners));
+});
