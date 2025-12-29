cd554663350354b082c72871c9c06fe68ccf5c95
krhoybraten <106737615+krhoybraten@users.noreply.github.com>
2025-12-29 18:37:51 +0100

    Create renderTags.js

---
+++ ui/renderTags.js
@@ -0,0 +1,30 @@
+export function renderTagCheckboxes({
+  container,
+  tags,
+  name = "tags",
+  onChange
+}) {
+  container.innerHTML = "";
+
+  for (const tag of tags) {
+    const id = `${name}-${tag}`;
+
+    const label = document.createElement("label");
+    label.style.display = "block";
+
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.name = name;
+    checkbox.value = tag;
+    checkbox.id = id;
+
+    if (onChange) {
+      checkbox.addEventListener("change", onChange);
+    }
+
+    label.appendChild(checkbox);
+    label.append(` ${tag}`);
+
+    container.appendChild(label);
+  }
+}
