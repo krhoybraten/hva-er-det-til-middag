981af70916c611382c6d40e81d220c270aba4611
krhoybraten <106737615+krhoybraten@users.noreply.github.com>
2025-12-29 12:17:18 +0100

    Create dinnerData.js

---
+++ modules/dinnerData.js
@@ -0,0 +1,12 @@
+const dinnerData = [
+  {name: 'Hamburger', tags: ['helg', 'oksekjøtt', 'rask']},
+  {name: 'Pizza', tags: ['helg']},
+  {name: 'Taco', tags: ['helg', 'oksekjøtt']},
+  {name: 'Fiskepinner', tags: ['fisk']},
+  {name: 'Fiskeburger', tags: ['fisk', 'rask']},
+  {name: 'Pølselapskaus', tags: ['svinekjøtt']},
+  {name: 'Fiskeboller', tags: ['fisk', 'rask']},
+  {name: 'Spaghetti med kjøttsaus', tags: ['oksekjøtt, 'rask']},
+  {name: 'Pasta Carbonara', tags: ['svinekjøtt, 'rask']},
+  {name: 'Fleskepannekake', tags: ['svinekjøtt']}
+];
