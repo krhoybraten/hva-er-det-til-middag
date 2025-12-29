4a3f9b7a109123fbb9417acca9cb72379eaf580b
krhoybraten <106737615+krhoybraten@users.noreply.github.com>
2025-12-29 19:26:56 +0100

    Replace random dinner generator with mock API implementation

--- main.js
+++ main.js
@@ -1,45 +1,29 @@
 import {dinnerData} from './data/dinnerData.js'
 import {getTags} from "./api/tags.js";
+import {getRandomDinner} from "./api/randomDinner.js";
 import {renderTagCheckboxes} from "./ui/renderTags.js";
 
 const middag = document.getElementById("middag");
 const generate = document.getElementById("generate");
 const tagContainer = document.getElementById("facets");
 const tags = await getTags();
+const selectedTags = []
 
 renderTagCheckboxes({
   tagContainer,
   tags,
   onChange: () => {
-    const selected = [...tagContainer.querySelectorAll("input:checked")]
+    selectedTags = [...tagContainer.querySelectorAll("input:checked")]
       .map(cb => cb.value);
 
     console.log("Selected tags:", selected);
   }
 });
 
-const dinners = [
-  'Hamburger',
-  'Pizza',
-  'Taco',
-  'Fiskepinner',
-  'Fiskeburger',
-  'Pølselapskaus',
-  'Fiskeboller',
-  'Spaghetti med kjøttsaus',
-  'Pasta Carbonara',
-  'Fleskepannekake'
-];
-
-function getRandomDinner(dinnerList) {
-  const dinnerIndex = Math.floor(Math.random() * dinnerList.length);
-  return dinnerList[dinnerIndex];
-}
-
 function replaceText(element, newText) {
   element.textContent = newText;
 }
 
 generate.addEventListener('click', () => {
-  replaceText(middag, getRandomDinner(dinners));
+  replaceText(middag, getRandomDinner(dinners, selected));
 });
