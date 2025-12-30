b45ae9149aee978dc59e43cfb4747ab3f8188445
krhoybraten <106737615+krhoybraten@users.noreply.github.com>
2025-12-30 13:05:22 +0100

    Bugfix main.js

--- main.js
+++ main.js
@@ -1,47 +1,50 @@
-import {dinnerData} from './data/dinnerData.js'
-import {getTags} from './api/tags.js';
-import {getRandomDinner} from './api/randomDinner.js';
-import {renderTagCheckboxes} from './ui/renderTags.js';
-import {renderDinnerResult} from './ui/renderDinnerResult.js';
+import { dinnerData } from './data/dinnerData.js'
+import { getTags } from './api/tags.js'
+import { getRandomDinner } from './api/randomDinner.js'
+import { renderTagCheckboxes } from './ui/renderTags.js'
+import { renderDinnerResult } from './ui/renderDinnerResult.js'
+import { getDinnersByTags } from './api/getDinnersByTags.js'
 
-const middag = document.getElementById('middag');
-const search = document.getElementbyId('search');
-const randomDinner = document.getElementById('random-dinner');
-const tagContainer = document.getElementById('facets');
-const dinnerResultContainer = document.getElementById('middag');
-const tags = await getTags();
-let selectedTags = [];
-let dinners = [];
+const middag = document.getElementById('middag')
+const search = document.getElementById('search')
+const randomDinnerBtn = document.getElementById('random-dinner')
+const tagContainer = document.getElementById('facets')
+const dinnerResultContainer = document.getElementById('middag')
+
+const tags = await getTags()
+let selectedTags = []
+let dinners = []
 
 renderTagCheckboxes({
   tagContainer,
   tags,
   onChange: () => {
     selectedTags = [...tagContainer.querySelectorAll("input:checked")]
-      .map(cb => cb.value);
-
-    console.log("Selected tags:", selectedTags);
+      .map(cb => cb.value)
   }
-});
+})
 
 function replaceText(element, newText) {
-  element.textContent = newText;
+  element.textContent = newText
 }
 
 search.addEventListener('click', () => {
-  const dinners = getdinnersByTags(selectedTags);
-  if(!dinnerResult.length) {
-    replaceText(middag, 'Fant ingen middager')
-  } else {
-    renderDinnerResult({dinnerResultContainer, dinners})
-  }
+  dinners = getDinnersByTags(selectedTags)
 
-randomDinner.addEventListener('click', () => {
-  const randomDinner = getRandomDinner(selectedTags);
-  if(!randomDinner) {
+  if (!dinners.length) {
     replaceText(middag, 'Fant ingen middager')
   } else {
-    dinners = [randomDinner]
-    renderDinnerResult({dinnerResultContainer, dinners});
+    renderDinnerResult({ dinnerResultContainer, dinners })
   }
-});
+})
+
+randomDinnerBtn.addEventListener('click', () => {
+  const one = getRandomDinner(selectedTags)
+
+  if (!one) {
+    replaceText(middag, 'Fant ingen middager')
+  } else {
+    dinners = [one]
+    renderDinnerResult({ dinnerResultContainer, dinners })
+  }
+})
