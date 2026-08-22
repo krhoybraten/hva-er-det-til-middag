325721d9c030b7c0f1ca0ef01443e2fc0f7c0493
krhoybraten <106737615+krhoybraten@users.noreply.github.com>
2026-08-22 22:06:02 +0200

    Add cannelloni dinner

--- data/dinnerData.js
+++ data/dinnerData.js
@@ -31,6 +31,7 @@ export const dinnerData = [
   { name: 'Pasta Carbonara', emoji: '🍝', tags: ['svinekjøtt', 'rask', 'pasta'], likedBy: likedBy(SLOTH) },
   { name: 'Pastagrateng med rigatoni', emoji: '🍝', tags: ['pasta'], likedBy: likedBy(SLOTH, KANGAROO), recipeUrls: [{name: 'Godt.no: Pastagrateng med rigatoni', url: 'https://www.godt.no/oppskrifter/pasta/8642/pasta-til-hele-familien'}] },
   { name: 'Pasta Alfredo', emoji: '🍝', tags: ['pasta'], likedBy: likedBy(SLOTH, KANGAROO), recipeUrls: [{name: 'Godt.no: Pasta Alfredo', url: 'https://www.godt.no/oppskrifter/pasta/4121/pasta-alfredo'}] },
+  { name: 'Cannelloni', emoji: '🍝', tags: ['pasta'], likedBy: likedBy(SLOTH, KANGAROO), recipeUrls: [{name: 'Godt.no: Cannelloni', url: 'https://www.godt.no/oppskrifter/pasta/4169/cannelloni'}] },
   { name: 'Fleskepannekake', emoji: '🥞', tags: ['svinekjøtt'], likedBy: likedBy(SLOTH, KANGAROO, FLYING_SQUIRREL) },
   { name: 'Pannekaker', emoji: '🥞', tags: ['svinekjøtt'], likedBy: likedBy(SLOTH, KANGAROO, FLYING_SQUIRREL) },
   { name: 'Rømmegrøt', emoji: '🥣', tags: ['vegetar'], likedBy: likedBy(FLYING_SQUIRREL) },
