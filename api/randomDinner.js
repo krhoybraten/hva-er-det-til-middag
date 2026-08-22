37f139a787eb2a07b8140a08d6eb31f2574bdcd1
krhoybraten <106737615+krhoybraten@users.noreply.github.com>
2026-08-22 20:25:22 +0200

    Pass family filters to random dinner

--- api/randomDinner.js
+++ api/randomDinner.js
@@ -1,6 +1,6 @@
-import {findRandomDinnerWithTags} from '../utils/dinnerUtils.js'
+import {findRandomDinner} from '../utils/dinnerUtils.js'
 import {dinnerData} from '../data/dinnerData.js'
 
-export function getRandomDinner(tags = []) {
-  return findRandomDinnerWithTags(dinnerData, tags)
+export function getRandomDinner(tags = [], familyMembers = []) {
+  return findRandomDinner(dinnerData, tags, familyMembers)
 }
