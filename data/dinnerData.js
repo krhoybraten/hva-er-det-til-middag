87dec11dff72b7635c3d4b8e21563430c00db87c
krhoybraten <106737615+krhoybraten@users.noreply.github.com>
2026-08-22 21:03:13 +0200

    Add lammelaar recipe link

--- data/dinnerData.js
+++ data/dinnerData.js
@@ -50,7 +50,7 @@ export const dinnerData = [
   { name: 'Butter Chicken', emoji: '🍛', tags: ['indisk', 'kylling', 'rask'], likedBy: likedBy() },
   { name: 'Korma', emoji: '🍛', tags: ['indisk', 'kylling', 'oksekjøtt', 'lammekjøtt', 'svinekjøtt'], likedBy: likedBy() },
   { name: 'Ramen', emoji: '🍜', tags: [], likedBy: likedBy(SLOTH) },
-  { name: 'Lammelår', emoji: '🍖', tags: ['lammekjøtt', 'helg', 'grill'], likedBy: likedBy(SLOTH) },
+  { name: 'Lammelår', emoji: '🍖', tags: ['lammekjøtt', 'helg', 'grill'], likedBy: likedBy(SLOTH), recipeUrls: [{name: 'Godt.no: Langtidsstekt lammelår', url: 'https://www.godt.no/oppskrifter/kjoett/lam/11180/langtidsstekt-lammelaar'}] },
   { name: 'Frossenpizza', emoji: '🍕', tags: ['svinekjøtt', 'oksekjøtt', 'rask'], likedBy: likedBy(SLOTH, KANGAROO, FLYING_SQUIRREL) },
   { name: 'Pasta med pesto', emoji: '🌿', tags: ['svinekjøtt', 'oksekjøtt', 'fisk', 'pasta', 'rask', 'vegetar'], likedBy: likedBy(KANGAROO) },
   { name: 'Chicken nuggets', emoji: '🍗', tags: ['kylling', 'rask'], likedBy: likedBy(SLOTH, KANGAROO, FLYING_SQUIRREL)},
